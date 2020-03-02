9724825f27898fbc826318eb5330c012b143c7a0
rurumimic <unhyop@gmail.com>
2020-03-02 22:25:37 +0900

    Initial Commit

---
+++ koo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  koo
+//
+//  Created by Dohyun Kim on 02/03/2020.
+//  Copyright © 2020 rurumimic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
